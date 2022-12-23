801cc64aef40bf1280e6bfb534a98039d042fe5c
akhtarraja513 <akhtarraja513@gmail.com>
2022-12-24 00:00:56 +0530

    Added more features

--- src/components/Section1.jsx/right/Right.jsx
+++ src/components/Section1.jsx/right/Right.jsx
@@ -5,74 +5,96 @@ import React, { useState } from 'react'
 const Right = () => {
     const [value, setValue] = useState("");
     const [showData, setShowData] = useState(true);
-    const userData = [
+    let userData = [
         {
+            id: 1,
             name: "Company Name",
             entity: "7 Entities",
             code: "Code",
             user: "60 User"
         },
         {
+            id: 2,
             name: "Company Name",
             entity: "5 Entities",
             code: "Code",
             user: "55 User"
         },
         {
+            id: 3,
             name: "Company Name",
             entity: "6 Entities",
             code: "Code",
             user: "59 User"
         },
         {
+            id: 4,
             name: "Company Name",
             entity: "4 Entities",
             code: "Code",
             user: "48 User"
         },
         {
+            id: 5,
             name: "Company Name",
             entity: "3 Entities",
             code: "Code",
             user: "57 User"
         },
         {
+            id: 6,
             name: "Company Name",
             entity: "1 Entities",
             code: "Code",
             user: "51 User"
         },
         {
+            id: 7,
             name: "Company Name",
             entity: "7 Entities",
             code: "Code",
             user: "61 User"
         },
         {
+            id: 8,
             name: "Company Name",
             entity: "3 Entities",
             code: "Code",
             user: "53 User"
         },
         {
+            id: 9,
             name: "Company Name",
             entity: "6 Entities",
             code: "Code",
             user: "49 User"
         },
         {
+            id: 10,
             name: "Company Name",
             entity: "5 Entities",
             code: "Code",
             user: "52 User"
         },
         {
+            id: 11,
             name: "Company Name",
             entity: "2 Entities",
             code: "Code",
             user: "42 User"
         }
     ]
+    const [data, setData] = useState(userData);
+    const handleSection = (idx) => {
+        // console.log(idx);
+        let findEle = userData.find((ele) => ele.id === idx)
+        // console.log(findEle);
+        findEle = {...findEle, name: "Mckinsey & Company", code: "MCKY"}
+        // console.log(findEle);
+        userData[findEle.id-1] = findEle
+        // console.log(userData);
+        setData(userData);
+    }
     return (
         <div className="right">
             <div className="right1">
@@ -92,15 +114,15 @@ const Right = () => {
                 </div>
             </div>
             {showData && <div className="right4">
-                {userData.filter((item) => {
+                {data.filter((item) => {
                     const searchName = value.toLowerCase();
                     const fullName = item.name.toLowerCase();
 
                     return (
                         fullName.startsWith(searchName)
                     )
-                }).map((ele, idx) => (
-                    <div className="right-4-sections" key={idx}>
+                }).map((ele) => (
+                    <div id={`${ele.id}`} className="right-4-sections" key={ele.id} onClick={() => handleSection(ele.id)} >
                         <div className="right-4-left">
                             <div className="content">{ele.name}</div>
                             <div className="quantity">{ele.entity}</div>
